649436cf89b636b271ae18e149ce73e26912f45d
anand231106 <nityagupta3210@gmail.com>
2024-03-25 00:06:08 +0530

    deploy

---
+++ js/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "My Lovely Bro",
+    introTitle: 'My Lovely Bro',
+    introDesc: `In life's unpredictable journey, you're my anchor,
+    Through highs and lows, you're my constant supporter.
+    Friend dear, your presence brings joy and laughter,
+    Together, we face challenges, happily ever after."`,
+    btnIntro: 'Enter My Herat',
+    title: '"You know, if friendship were a game, I would pick you as my teammate without a second thought!"',
+    desc: 'Would you like to be my teammate in this game?',
+    btnYes: 'Yes, of course',
+    btnNo: 'No',
+    question: 'With over 7 billion people in the world, why do you choose me <3',
+    btnReply: 'Send to Nitya <3',
+    reply: 'Because you seem the best in behavior, and when I am sick, I feel good putting your hand on my head..........',
+    mess: 'I know 🥰. Love you lots 😘😘',
+    messDesc: 'Thanks bro ! I am excited to join you on  Insta . So, click on Ok and wait...........',
+    btnAccept: 'Okiiiii <3',
+    messLink: 'https://www.instagram.com/_nityanand.gupta?igsh=MTM2ZHhzN2RsdHUzeg==' 
+}
